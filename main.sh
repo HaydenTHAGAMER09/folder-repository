e585a940d9a202ff4a4304de3a2d17d007ed6869
HaydenTHAGAMER09 <154368692+HaydenTHAGAMER09@users.noreply.github.com>
2023-12-19 16:11:47 -0500

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,9 @@
+echo "Hello! This is the main.sh file, capable of coding bash."
+sleep 5
+echo "Here is an input: "
+sleep 2
+read -p "Input: " REPLY
+echo "And now that you've inputed, your text was...
+sleep 3
+sleep 3
+echo "$REPLY !"
